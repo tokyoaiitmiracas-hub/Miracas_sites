c9d6242fe083f061187210ed8cae7bfa2121fe76
藤井蒼太 <vga29-pc250029@sankogakuen.jp>
2026-05-25 15:32:39 +0900

    User削除 藤井・石戸

--- admin.js
+++ admin.js
@@ -4,8 +4,7 @@ const ADMIN_EMAILS = new Set([
   "vga29-pc250029@sankogakuen.jp",
   "12210295@sankogakuen.jp",
   "12010311@sankogakuen.jp",
-  "kousei10160926@gmail.com",
-  "sohuta0810@gmail.com"
+  "kousei10160926@gmail.com"
 ]);
 
 export function isAdmin(email){
--- setting.js
+++ setting.js
@@ -27,8 +27,7 @@ const adminEmails = [
     "vga29-pc250029@sankogakuen.jp",
     "12210295@sankogakuen.jp",
     "12010311@sankogakuen.jp",
-    "kousei10160926@gmail.com",
-    "sohuta0810@gmail.com"
+    "kousei10160926@gmail.com"
 ];
 
 onAuthStateChanged(auth, (user)=>{
